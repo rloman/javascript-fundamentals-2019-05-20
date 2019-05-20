dcda1cfd9d9c183847e640ff4725bab4fb5bddbd
Raymond Loman <raymond@carpago.nl>
2019-05-20 14:45:59 +0200

    Add if else and switch

---
+++ controlstructures/if/js/main.js
@@ -0,0 +1,48 @@
+"use strict"
+
+
+function demo() {
+
+    let age = 50;
+
+    if (age >= 40) { // then => 
+        console.log("U bent senior!!!");
+    }
+    else {
+        if (age < 30) {
+            console.log("U bent junior!!!");
+        }
+        else {
+            console.log("genoeg ... ")
+        }
+    }
+
+    // ander voorbeeld
+
+    let male = true; // mannetje
+
+    console.log(!male);
+
+    if (male) {
+
+    }
+
+    if (male == true) {
+
+    }
+
+    age = 60;
+    if (age != 60) {
+
+    }
+
+    let name = "Ray";
+
+    if (name != "John") {
+
+    }
+
+
+}
+
+
