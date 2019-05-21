2ec2f89f66a651af61629bffaac1d350fd9a813c
Raymond Loman <raymond@carpago.nl>
2019-05-21 16:05:59 +0200

    Add demo code for dom

--- dom/js/main.js
+++ dom/js/main.js
@@ -2,6 +2,16 @@
 
 
 function demo() {
-    console.log(document.getElementById("address").value);
+    console.log(window.document.data.address.value);
+    console.log(document.getElementById("addressId").value);
+    console.log(document.forms[1].address.value);
+}
+
+function yellow() {
+    let divList = document.getElementsByTagName("div");
+    for(let divje of divList) {
+        divje.style.backgroundColor="yellow";
+        divje.style.fontWeight="bold";
+    }
 }
 
