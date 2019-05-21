53cd7844131c82634ce0b747588cabc5ebb6456a
Raymond Loman <raymond@carpago.nl>
2019-05-21 15:39:03 +0200

    Werkend

---
+++ dom/js/main.js
@@ -0,0 +1,7 @@
+"use strict"
+
+
+function demo() {
+    console.log(document.getElementById("address").value);
+}
+
--- objects/js/main.js
+++ objects/js/main.js
@@ -1,14 +1,18 @@
 "use strict"
-
-
-
 {
     // create an Object the old(er) way
-    var person = new Object;
+    let person = new Object();
     person.firstName = "John";
     person.lastName = "Smith";
+    person.age = 51;
 
     console.log(person.firstName);
+    console.log("De persoon heeft nu een leeftijd van: "
+    +person.age);
+
+    // is hetzelfde als
+    console.log("De persoon heeft nu een leeftijd van: "
+    +person["age"]);
 }
 
 {
@@ -16,9 +20,9 @@
     // Why and When? When the name of the
     // property is not a valid id e.g. 2names, 3whatever
     // e.g.
-
     var person = new Object;
     person["3name"] = "Poedel";
+    // person.3name = "J"; // vout
 
     console.log(person["3name"]);
 }
@@ -29,7 +33,7 @@
 
 {
     // create an Object the new(er) way
-    let person = {};
+    let person = {}; // identiek aan new Object();
     person.firstName = "John";
     person.lastName = "Smith";
 
@@ -54,9 +58,21 @@
     //using for in ...
     var person = new Object;
     person.name = "Jane";
-    person.address = "Daltonlaan 700";
+    person.age = 45;
+    person.hobbies = ["Dansen", "Zingen", "Bier drinken!"];
+    person.address = {
+        street: "Daltonlaan",
+        number: 700,
+        zipCode: "8883AA"
+    }
 
-    for(let key in person) {
-        console.log("The "+key+"'s value is:"+person[key]);
+    for (let key in person) {
+        console.log("The " + key + "'s value is:" + person[key]);
+        if(person[key] instanceof Object) {
+            let address = person[key];
+            for(let key2 in address) {
+                console.log(address[key2]);
+            }
+        }
     }
 }
