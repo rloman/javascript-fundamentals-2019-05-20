0db5a57d5427e0ac96b5c2c7a16ab33612b1d688
Raymond Loman <raymond@carpago.nl>
2019-05-22 13:05:51 +0200

    Add element to focus

--- validation/js/main.js
+++ validation/js/main.js
@@ -3,22 +3,30 @@
 
 function formSubmit() { // returns a boolean
 
-    let messages ="";
+    let messages = "";
+
+
+    let elementToFocus;
 
     let inputFirstName = document.getElementById("firstName");
-    if(inputFirstName.value.trim() == "") {
-       messages += "Please supply a value for firstName";
-       inputFirstName.focus(); // to set focus to inputFirstName
+    if (inputFirstName.value.trim() == "") {
+        messages += "Please supply a value for firstName";
+        elementToFocus = inputFirstName;
     }
 
-    if(document.getElementById("email").value.trim() == "") {
+    if (document.getElementById("email").value.trim() == "") {
         messages += "\nPlease supply a value for email";
+
+        if (!elementToFocus) {
+            elementToFocus = document.getElementById("email");
+        }
     }
 
     // finally
 
-    if(messages != "") {
+    if (messages != "") {
         alert(messages);
+        elementToFocus.focus();
 
         return false;
     }
